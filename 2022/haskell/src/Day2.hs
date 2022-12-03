385f1a81a856676e97b2db6de7f682f7556e2e6a
Sebastian Callh <sebastian.callh@northvolt.com>
2022-12-03 16:10:18 +0100

    Add Haskell day2

---
+++ 2022/haskell/src/Day2.hs
@@ -0,0 +1,39 @@
+module Day2 where
+
+import Data.Text as T
+
+ints "A" = 1
+ints "B" = 2
+ints "C" = 3
+ints "X" = 1
+ints "Y" = 2
+ints "Z" = 3
+
+winning 3 = 1
+winning n = succ n
+losing 1 = 3
+losing n = pred n
+
+parseRounds :: T.Text -> [(Int, Int)]
+parseRounds contents =
+  (\(op:me:[]) -> (ints op, ints me)) <$> splits
+  where
+    splits = T.splitOn " " <$> T.lines contents
+
+roundScore (op, me)
+  | op == me = 3
+  | winning op == me = 6
+  | otherwise = 0
+
+shapeScore (_, me) = me
+
+score = fmap (\r -> roundScore r + shapeScore r)
+
+strategy (op, me)
+  | me == 1 = (op, losing op)
+  | me == 2 = (op, op)
+  | me == 3 = (op, winning op)
+
+sol1 = sum . score . parseRounds
+
+sol2 = sum . score . fmap strategy . parseRounds
--- 2022/haskell/src/Main.hs
+++ 2022/haskell/src/Main.hs
@@ -2,10 +2,20 @@ module Main where
 import qualified Data.Text.IO as T
 
 import Day1
+import Day2
 
 main :: IO ()
 main = do
-  contents <- T.readFile "../input/1.txt"
+  T.readFile "../input/1.txt" >>= day1
+  T.readFile "../input/2.txt" >>= day2
+
+day1 contents = do
   putStrLn "---- Day 1 ----"
-  putStrLn . show $  Day1.sol1 contents
+  putStrLn . show $ Day1.sol1 contents
   putStrLn . show $ Day1.sol2 contents
+
+
+day2 contents = do
+  putStrLn "---- Day 2 ----"
+  putStrLn . show $ Day2.sol1 contents
+  putStrLn . show $ Day2.sol2 contents
