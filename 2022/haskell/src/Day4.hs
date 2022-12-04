9d8e3cf5704a1a445d77ad664295de87203a07be
Sebastian Callh <sebastian.callh@northvolt.com>
2022-12-04 12:45:49 +0100

    Add Haskell day 4

---
+++ 2022/haskell/src/Day4.hs
@@ -0,0 +1,28 @@
+module Day4 where
+
+import Data.Either
+import qualified Data.Text as T
+import qualified Data.Text.Read as T
+
+parse line = ((int from1, int to1), (int from2, int to2))
+  where
+    [from1, to1] = T.splitOn "-" elf1
+    [from2, to2] = T.splitOn "-" elf2
+    [elf1, elf2] = T.splitOn "," line
+    int = fromRight 0 . fmap fst . T.decimal
+
+subsets ((min1, max1), (min2, max2)) =
+  (min1 <= min2 && max2 <= max1) ||
+  (min2 <= min1 && max1 <= max2)
+
+overlaps ((min1, max1), (min2, max2)) =
+  (min1 <= min2 && min2 <= max1) ||
+  (min2 <= min1 && min1 <= max2) ||
+  (min1 <= max2 && max2 <= max1) ||
+  (min2 <= max1 && max1 <= max2)
+
+toint True = 1
+toint False = 0
+
+sol1 = sum . fmap (toint . subsets . parse) . T.lines
+sol2 = sum . fmap (toint . overlaps . parse) . T.lines
--- 2022/haskell/src/Main.hs
+++ 2022/haskell/src/Main.hs
@@ -4,12 +4,14 @@ import qualified Data.Text.IO as T
 import Day1
 import Day2
 import Day3
+import Day4
 
 main :: IO ()
 main = do
   T.readFile "../input/1.txt" >>= day1
   T.readFile "../input/2.txt" >>= day2
   T.readFile "../input/3.txt" >>= day3
+  T.readFile "../input/4.txt" >>= day4
 
 day1 contents = do
   putStrLn "---- Day 1 ----"
@@ -25,3 +27,8 @@ day3 contents = do
   putStrLn "---- Day 3 ----"
   putStrLn . show $ Day3.sol1 contents
   putStrLn . show $ Day3.sol2 contents
+
+day4 contents = do
+  putStrLn "---- Day 4 ----"
+  putStrLn . show $ Day4.sol1 contents
+  putStrLn . show $ Day4.sol2 contents
